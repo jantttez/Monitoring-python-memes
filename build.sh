0506a24afea12ce896f5b7c4005f86e2d7f4a7ac
jantttez <kirillzuev987@gmail.com>
2024-05-21 19:02:04 +0800

    commit at date: Вт 21 мая 2024 14:02:04 +08

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 SECOND_TO_TEST=5
 PORT=8090
 
@@ -7,7 +8,8 @@ docker build -t app:monitoring . -f Dockerfile
 docker run -p ${PORT}:8000 -d --name monitoring --rm app:monitoring
 
 
-echo "Go to your web browset and link to localhost:${PORT}"
+echo "Go to your web browseк and link to localhost:${PORT}"
+
 echo "You have ${SECOND_TO_TEST} second to test"
 
 sleep ${SECOND_TO_TEST}

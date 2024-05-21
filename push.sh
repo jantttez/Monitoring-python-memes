b78c894c495b81ccc61d949e80c6cffb86587a3b
jantttez <kirillzuev987@gmail.com>
2024-05-21 19:05:19 +0800

    commit at date: Вт 21 мая 2024 14:05:19 +08

--- push.sh
+++ push.sh
@@ -9,7 +9,9 @@ git status -s > status.txt
 
 LINE_COUNT=$(wc -l < "$FILE_PATH")
 
-if [ $LINE_COUNT -eq 1 ]; then
+LINE=$(git status -s | awk '{print $1}')
+
+if [ ! -n $LINE ]; then
   echo "нету изменений ( no changes found)"
 else 
   echo "Pushing new localization changes"
@@ -20,3 +22,6 @@ else
 fi
 
 #в дальнейшем можно разтурбировать чтоб в новую бранчу комитить
+
+
+#if [ $LINE_COUNT -eq 1 ]; then
